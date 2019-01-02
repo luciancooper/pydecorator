da6ac1ea048d762e24f7727821b0008eb761a308
Lucian Cooper <cooperlucian@gmail.com>
2019-01-02 18:21:04 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='pydecorator',
+    version='1.0',
+    author='Lucian Cooper',
+    url='https://github.com/luciancooper/pydecorator',
+    description='Python Decorator Functions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='decorator utility',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+    packages=['pydecorator'],
+    install_requires=['numpy','pandas'],
+)
